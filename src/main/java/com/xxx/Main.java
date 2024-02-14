5b4319971ba949745668ff8e0ca1245b3573e3db
lzq <lzq@qq.com>
2024-02-14 16:48:19 +0800

    1 commit

---
+++ src/main/java/com/xxx/Main.java
@@ -0,0 +1,11 @@
+package com.xxx;
+
+/**
+ * @author 刘志强
+ * @version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
