c72a9de90e37a9cd80c59684bed42de7adb0b57a
lzq <lzq@qq.com>
2024-02-14 17:09:03 +0800

    hot-fix commit-1

--- src/main/java/com/xxx/Main.java
+++ src/main/java/com/xxx/Main.java
@@ -8,5 +8,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("2222222");
+        System.out.println("33333333");
     }
 }
