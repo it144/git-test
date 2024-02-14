36a710533bf75cb4667471cfb606dd23ede7dc24
lzq <lzq@qq.com>
2024-02-14 17:14:29 +0800

    master commit-1

--- src/main/java/com/xxx/Main.java
+++ src/main/java/com/xxx/Main.java
@@ -9,5 +9,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("2222222");
         System.out.println("33333333");
+        System.out.println("master test");
     }
 }
